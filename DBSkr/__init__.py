2b205576c3f28d74c59957892121ee5c9b0ad16c
gunyu1019 <gunyu1019@gmail.com>
2021-01-15 11:33:15 +0900

    Last file commit

--- DBSkr/__init__.py
+++ DBSkr/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 """
 Discord Bot-lists-api Service
 ~~~~~~~~~~~~~~~~~~~
@@ -13,13 +12,7 @@ __author__ = 'gunyu1019'
 __license__ = 'MIT'
 __version__ = '0.1'
 
-#from collections import namedtuple
-
 from .client import client
 from .errors import *
 from .model import *
 from .http import httpClient
-
-#VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
-
-#version_info = VersionInfo(major = 0, minor = 3, micro = 4, releaselevel = 'final', serial = 0)
--- setup.py
+++ setup.py
@@ -4,10 +4,21 @@ setup(
     name='DBSKR',
     version='v1.0',
     packages=['DBSkr'],
-    url='',
+    url='https://github.com/gunyu1019/DBSkr-py',
     license='MIT',
     author='gunyu1019',
     author_email='gunyu1019@gmail.com',
     description='Koreanbots와 top.gg를 위한 비공식 파이썬 API 레퍼입니다.',
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=open('requirements.txt').read(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
 )
