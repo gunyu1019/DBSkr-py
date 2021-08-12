be4462a274abbec044a0af5c80a2fa2dfeca799e
gunyu1019 <gunyu1019@gmail.com>
2021-08-12 14:16:06 +0900

    test-client..?

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,5 +5,4 @@ import os
 
 @pytest.mark.asyncio
 async def test_client():
-    print(os.environ.get('KEY'))
-    return os.environ.get('KEY') is not None
+    raise TypeError(os.environ.get('KEY'))
